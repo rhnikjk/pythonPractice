353a0a4d461de4b7ab31b347d491d81e3350e1f6
rhnikjk <mccroner@wsc.school.nz>
2024-06-06 05:39:44 +1200

    Update TOp100.py

--- TOp100.py
+++ TOp100.py
@@ -1,6 +1,7 @@
 b=1
 guesses = []
 ANSWERS = ["Russia","Canada","China","USA","Brazil","Australia","India","Argentina","Kazakhstan","Algeria","Congo","Greenland","Saudi Arabia","Mexico","Indonesia","Sudan","Libya","Iran","Mongolia","Peru","Chad","Niger","Angola","Mali","South Africa","Colombia","Ethiopia","Bolivia","Mauritania","Egypt","Tanzania","Nigeria","Venezuela","Pakistan","Namibia","Mozambique","Turkey","Chile","Zambia","Myanmar","Afghanistan","Somalia","Central African Republic","South Sudan","Ukraine","Madagascar","Botswana","Kenya","France","Yemen"]
+#-----------------------FUNCTIONS----------------------------------------
 def intro():
     print("hello welcome to the quiz")
     name = input ("hello, what is your name? ")
@@ -24,18 +25,19 @@ def inlist(answer,list):
     else:
         return False
 
-
+#--------------------------MAIN CODE-----------------------------------
 intro()
 lives = getlives()
 score = 0
 
-for i in range(len(ANSWERS)):
-    i=b
-    x=i+1
-    answer = input("name one of the 50 biggest contrys\n")
-    if inlist(answer,ANSWERS):
-        print("correct")
-        b += 1
-    else:
-        print("worng")
-        b = 0
+while lives > 0:
+    for i in range(len(ANSWERS)):
+        i=b
+        x=i+1
+        answer = input("name one of the 50 biggest contrys\n")
+        if inlist(answer,ANSWERS) and inlist(answer,guesses):
+            print("correct")
+            b += 1
+        else:
+            print("worng")
+            b = 0
